011dbc15d52479fa2730dfbd2761ef005e2ca4c4
adaephonben <ma18btech11011@iith.ac.in>
2019-09-07 23:52:45 +0530

    GHPages weird thing

---
+++ precache-manifest.74eca88f7dc3b213074178d73e8faf8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29d5bc6e94e4c80d182b768bf69a7f93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f23973c88f34a0d8792a",
+    "url": "/static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "921591a96ab9a0496a0c",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f23973c88f34a0d8792a",
+    "url": "/static/js/2.23bfabd5.chunk.js"
+  },
+  {
+    "revision": "921591a96ab9a0496a0c",
+    "url": "/static/js/main.78053ece.chunk.js"
+  },
+  {
+    "revision": "0104ed06225b1fa4760c",
+    "url": "/static/js/runtime~main.f50b8f44.js"
+  }
+]);
